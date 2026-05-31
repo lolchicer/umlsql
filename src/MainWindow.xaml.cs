078316eafba46ad25bb07ed38767b44854467f99
lolchicer <lolchicer@live.ru>
2026-05-31 14:55:42 +0300

    save connectionstring

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Lolchicer.Umlsql.View;
 using Lolchicer.Umlsql.ViewModel;
+using Microsoft.VisualBasic;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,6 +15,15 @@ public partial class MainWindow : Window
 {
     public MainWindow()
     {
+        var connectionContext = new ConnectionContext()
+        { ConnectionString = "Host=localhost;Port=5432;Database=usersdb;Username=username;Password=password" };
+
+        if (File.Exists(@"ConnectionString.txt"))
+            connectionContext = new ConnectionContext()
+            { ConnectionString = File.ReadAllText(@"ConnectionString.txt") };
+
+        Resources.Add("ConnectionContext", connectionContext);
+
         InitializeComponent();
     }
 
@@ -23,10 +34,18 @@ public partial class MainWindow : Window
 
     private void button_Click(object sender, RoutedEventArgs e)
     {
-        var interfaceWindow = new ProductWindow(((ConnectionContext)this.FindResource("ConnectionContextMain")).ConnectionString)
+        var interfaceWindow = new ProductWindow(((ConnectionContext)this.FindResource("ConnectionContext")).ConnectionString)
         {
             Owner = this
         };
         interfaceWindow.Show();
+    }
+
+    private void SaveConnectionString(object sender, RoutedEventArgs e)
+    {
+        File.WriteAllText(
+            @"ConnectionString.txt",
+            ((ConnectionContext)this.FindResource("ConnectionContext")).ConnectionString
+        );
     }
 }
