a7305900f95dc2ddac14a1b826d240155f1c3b6f
lolchicer <lolchicer@live.ru>
2026-06-27 18:48:35 +0300

    Revert "ApplicationContext; ConnectionContext"
    This reverts commit 59bdb28897e0359b87a48bf7250a66149adbdced.

--- src/view/InterfaceWindow.xaml.cs
+++ src/view/InterfaceWindow.xaml.cs
@@ -2,34 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 
-namespace Lolchicer.Umlsql.View;
-
-public partial class InterfaceWindow : Window
+namespace Lolchicer.Umlsql.View
 {
-    private ConnectionContext _connectionContext;
-
-    private void InterfaceWindow_Loaded(object sender, RoutedEventArgs e)
+    public partial class InterfaceWindow : Window
     {
-        // Make a new data source object
-        var applicationContext = new ApplicationContext()
+        public InterfaceWindow()
         {
-            ConnectionString = _connectionContext.ConnectionString
-        };
-
-        // New binding object using the path of 'Name' for whatever source object is used
-        var ApplicationContextMain = new Binding("ConnectionString");
-
-        // Configure the binding
-        ApplicationContextMain.Mode = BindingMode.OneWay;
-        ApplicationContextMain.Source = applicationContext;
-    }
-
-    public InterfaceWindow(ConnectionContext connectionContext)
-    {
-        _connectionContext = connectionContext;
-        InitializeComponent();
+            InitializeComponent();
+        }
     }
 }
