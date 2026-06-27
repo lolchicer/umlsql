3f5a46392a06be027f3ace750b38123b66e979bc
lolchicer <lolchicer@live.ru>
2026-06-27 18:48:03 +0300

    Revert "исправление обновления ConnectionString"
    This reverts commit 82c26323467717b52473a90bdcf6dc8974ea5c75.

--- src/ConnectionContext.cs
+++ src/ConnectionContext.cs
@@ -15,7 +15,7 @@ public class ConnectionContext : INotifyPropertyChanged
         set
         {
             _connectionString = value;
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ConnectionString)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PropertyChanged)));
         }
     }
 }
