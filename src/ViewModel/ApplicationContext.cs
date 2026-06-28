0f25302b909a3be82d040c186db8811fb62e647e
lolchicer <lolchicer@live.ru>
2026-06-28 16:17:45 +0300

    ApplicationContext

---
+++ src/ViewModel/ApplicationContext.cs
@@ -0,0 +1,37 @@
+﻿using Lolchicer.Umlsql.ViewModel;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lolchicer.Umlsql.ViewModel;
+
+public class ApplicationContext : DbContext, INotifyPropertyChanged
+{
+    private string _connectionString = string.Empty;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public string ConnectionString
+    {
+        get => _connectionString;
+        set
+        {
+            _connectionString = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PropertyChanged)));
+            Database.EnsureCreated();
+        }
+    }
+
+    public DbSet<Interface> Interfaces { get; set; } = null!;
+
+    public DbSet<Getproperty> Getproperties { get; set; } = null!;
+
+    public DbSet<Setproperty> Setproperties { get; set; } = null!;
+
+    public DbSet<Argument> Arguments { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_connectionString);
+    }
+}
