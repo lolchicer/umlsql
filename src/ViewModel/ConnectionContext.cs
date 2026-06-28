615d21b03eca5985dfe50bccc50564095a82b885
lolchicer <lolchicer@live.ru>
2026-06-28 16:17:51 +0300

    ConnectionContext

---
+++ src/ViewModel/ConnectionContext.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace Lolchicer.Umlsql.ViewModel;
+
+public class ConnectionContext : INotifyPropertyChanged
+{
+    private string _connectionString = string.Empty;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public required string ConnectionString
+    {
+        get => _connectionString;
+        set
+        {
+            _connectionString = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ConnectionString)));
+        }
+    }
+
+    public ApplicationContext ApplicationContext
+    {
+        get => new()
+        {
+            ConnectionString = ConnectionString
+        };
+    }
+}
