678b794e4c6f08ca75bce1e75762b76716b571b7
lolchicer <lolchicer@live.ru>
2026-07-26 17:40:25 +0300

    переориентация ApplicationContext под 2.0.0.0

--- src/ViewModel/ApplicationContext.cs
+++ src/ViewModel/ApplicationContext.cs
@@ -40,32 +40,29 @@ public class ApplicationContext : DbContext, INotifyPropertyChanged
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Interface>()
-            .HasKey(@interface => @interface.Id);
-
-        modelBuilder.Entity<Method>()
-            .HasKey(method => new { method.Id, method.InterfaceId });
+        modelBuilder.Entity<Function>()
+            .HasKey(function => function.Id);
 
         modelBuilder.Entity<Argument>()
-            .HasKey(argument => new { argument.Id, argument.MethodId, argument.InterfaceId });
+            .HasKey(argument => new { argument.Id, argument.FunctionId });
+
+        modelBuilder.Entity<Interface>()
+            .HasKey(@interface => new { @interface.Id, @interface.FunctionId });
+
+        modelBuilder.Entity<Method>()
+            .HasKey(method => new { method.Id, method.ArgumentId, method.FunctionId });
 
         modelBuilder.Entity<Getproperty>()
-            .HasKey(getproperty => new { getproperty.Id, getproperty.MethodId });
+            .HasKey(getproperty => new { getproperty.Id, getproperty.MethodId, getproperty.ArgumentId, getproperty.FunctionId });
 
         modelBuilder.Entity<Setproperty>()
-            .HasKey(setproperty => new { setproperty.Id, setproperty.MethodId });
-
-        modelBuilder.Entity<Method>()
-            .HasOne(method => method.Argument)
-            .WithMany(@interface => @interface.Methods)
-            .HasForeignKey(method => method.InterfaceId)
-            .HasPrincipalKey(@interface => @interface.Id);
+            .HasKey(setproperty => new { setproperty.Id, setproperty.MethodId, setproperty.ArgumentId, setproperty.FunctionId });
 
         modelBuilder.Entity<Argument>()
-            .HasOne(argument => argument.Method)
-            .WithMany(method => method.Arguments)
-            .HasForeignKey(argument => new { argument.MethodId, argument.InterfaceId })
-            .HasPrincipalKey(method => new { method.Id, method.InterfaceId });
+            .HasOne(argument => argument.Function)
+            .WithMany(function => function.Arguments)
+            .HasForeignKey(argument => argument.FunctionId)
+            .HasPrincipalKey(function => function.Id);
 
         modelBuilder.Entity<Argument>()
             .HasOne(argument => argument.Type)
@@ -73,22 +70,28 @@ public class ApplicationContext : DbContext, INotifyPropertyChanged
             .HasForeignKey(argument => argument.TypeId)
             .HasPrincipalKey(type => type.Id);
 
+        modelBuilder.Entity<Interface>()
+            .HasOne(@interface => @interface.Function)
+            .WithMany(function => function.Interfaces)
+            .HasForeignKey(@interface => @interface.FunctionId)
+            .HasPrincipalKey(function => function.Id);
+
+        modelBuilder.Entity<Method>()
+            .HasOne(method => method.Argument)
+            .WithMany(argument => argument.Methods)
+            .HasForeignKey(method => new { method.ArgumentId, method.FunctionId })
+            .HasPrincipalKey(argument => new { argument.Id, argument.FunctionId });
+
         modelBuilder.Entity<Getproperty>()
             .HasOne(getproperty => getproperty.Method)
             .WithMany(method => method.Getproperties)
-            .HasForeignKey(getproperty => new { getproperty.MethodId, getproperty.InterfaceId })
-            .HasPrincipalKey(method => new { method.Id, method.InterfaceId });
-
-        modelBuilder.Entity<Getproperty>()
-            .HasOne(getproperty => getproperty.Type)
-            .WithMany(type => type.Getproperties)
-            .HasForeignKey(getproperty => getproperty.TypeId)
-            .HasPrincipalKey(type => type.Id);
+            .HasForeignKey(getproperty => new { getproperty.MethodId, getproperty.ArgumentId, getproperty.FunctionId })
+            .HasPrincipalKey(method => new { method.Id, method.ArgumentId, method.FunctionId });
 
         modelBuilder.Entity<Setproperty>()
             .HasOne(setproperty => setproperty.Method)
             .WithMany(method => method.Setproperties)
-            .HasForeignKey(setproperty => new { setproperty.MethodId, setproperty.InterfaceId })
-            .HasPrincipalKey(method => new { method.Id, method.InterfaceId });
+            .HasForeignKey(setproperty => new { setproperty.MethodId, setproperty.ArgumentId, setproperty.FunctionId })
+            .HasPrincipalKey(method => new { method.Id, method.ArgumentId, method.FunctionId });
     }
 }
