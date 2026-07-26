aa5e27ff6398a752a8cf9500dc26ad81c8dae550
lolchicer <lolchicer@live.ru>
2026-07-26 17:50:18 +0300

    RowPage.xaml.cs

---
+++ src/View/RowPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Lolchicer.Umlsql.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lolchicer.Umlsql.View
+{
+    /// <summary>
+    /// Логика взаимодействия для RowPage.xaml
+    /// </summary>
+    public partial class RowPage : Page
+    {
+        public RowPage(IRow row)
+        {
+            Resources.Add("Argument", row);
+
+            InitializeComponent();
+        }
+    }
+}
