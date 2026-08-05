53b9e81d4bb5825ef4a34bc4cb7868f65d0110ca
lolchicer <lolchicer@live.ru>
2026-08-05 21:07:03 +0300

    инкапсуляция энтитей

--- src/ViewModel/ApplicationContext.cs
+++ src/ViewModel/ApplicationContext.cs
@@ -39,16 +39,15 @@ public class ApplicationContext : DbContext, INotifyPropertyChanged
 
     public DbSet<Setproperty> Setproperties { get; set; } = null!;
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseNpgsql(_connectionString);
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // великий и могучий технический язык, который я не знаю.
+    private void CreateModel(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Model>()
             .HasKey(model => model.Id);
+    }
 
+    private void CreateView(ModelBuilder modelBuilder)
+    {
         modelBuilder.Entity<View>()
             .HasKey(view => new { view.Id, view.ModelId });
 
@@ -63,24 +62,18 @@ public class ApplicationContext : DbContext, INotifyPropertyChanged
             .WithMany(type => type.ViewTypes)
             .HasForeignKey(view => view.TypeId)
             .HasPrincipalKey(type => type.Id);
+    }
 
+    private void CreateFunction(ModelBuilder modelBuilder)
+    {
         modelBuilder.Entity<Function>()
             .HasKey(function => function.Id);
+    }
 
+    private void CreateArgument(ModelBuilder modelBuilder)
+    {
         modelBuilder.Entity<Argument>()
             .HasKey(argument => new { argument.Id, argument.FunctionId });
-
-        modelBuilder.Entity<Interface>()
-            .HasKey(@interface => new { @interface.Id, @interface.FunctionId });
-
-        modelBuilder.Entity<Method>()
-            .HasKey(method => new { method.Id, method.ArgumentId, method.FunctionId });
-
-        modelBuilder.Entity<Getproperty>()
-            .HasKey(getproperty => new { getproperty.Id, getproperty.MethodId, getproperty.ArgumentId, getproperty.FunctionId });
-
-        modelBuilder.Entity<Setproperty>()
-            .HasKey(setproperty => new { setproperty.Id, setproperty.MethodId, setproperty.ArgumentId, setproperty.FunctionId });
 
         modelBuilder.Entity<Argument>()
             .HasOne(argument => argument.Function)
@@ -93,29 +86,70 @@ public class ApplicationContext : DbContext, INotifyPropertyChanged
             .WithMany(type => type.ArgumentTypes)
             .HasForeignKey(argument => argument.TypeId)
             .HasPrincipalKey(type => type.Id);
+    }
+
+    private void CreateInterface(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Interface>()
+            .HasKey(@interface => new { @interface.Id, @interface.FunctionId });
 
         modelBuilder.Entity<Interface>()
             .HasOne(@interface => @interface.Function)
             .WithMany(function => function.Interfaces)
             .HasForeignKey(@interface => @interface.FunctionId)
             .HasPrincipalKey(function => function.Id);
+    }
+
+    private void CreateMethod(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Method>()
+            .HasKey(method => new { method.Id, method.ArgumentId, method.FunctionId });
 
         modelBuilder.Entity<Method>()
             .HasOne(method => method.Argument)
             .WithMany(argument => argument.Methods)
             .HasForeignKey(method => new { method.ArgumentId, method.FunctionId })
             .HasPrincipalKey(argument => new { argument.Id, argument.FunctionId });
+    }
+
+    private void CreateGetproperty(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Getproperty>()
+            .HasKey(getproperty => new { getproperty.Id, getproperty.MethodId, getproperty.ArgumentId, getproperty.FunctionId });
 
         modelBuilder.Entity<Getproperty>()
             .HasOne(getproperty => getproperty.Method)
             .WithMany(method => method.Getproperties)
             .HasForeignKey(getproperty => new { getproperty.MethodId, getproperty.ArgumentId, getproperty.FunctionId })
             .HasPrincipalKey(method => new { method.Id, method.ArgumentId, method.FunctionId });
+    }
+
+    private void CreateSetproperty(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Setproperty>()
+            .HasKey(setproperty => new { setproperty.Id, setproperty.MethodId, setproperty.ArgumentId, setproperty.FunctionId });
 
         modelBuilder.Entity<Setproperty>()
             .HasOne(setproperty => setproperty.Method)
             .WithMany(method => method.Setproperties)
             .HasForeignKey(setproperty => new { setproperty.MethodId, setproperty.ArgumentId, setproperty.FunctionId })
             .HasPrincipalKey(method => new { method.Id, method.ArgumentId, method.FunctionId });
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        CreateModel(modelBuilder);
+        CreateView(modelBuilder);
+        CreateFunction(modelBuilder);
+        CreateArgument(modelBuilder);
+        CreateInterface(modelBuilder);
+        CreateMethod(modelBuilder);
+        CreateGetproperty(modelBuilder);
+        CreateSetproperty(modelBuilder);
     }
 }
