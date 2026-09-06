630781b91079a8bf9a31594580474e6f5a32ba3e
lolchicer <lolchicer@live.ru>
2026-09-06 22:26:03 +0300

    заменил CardIdBoxTuple

---
+++ src/View/Controls/IdBoxTuple.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+
+namespace Lolchicer.Umlsql.View.Controls;
+
+public class IdBoxTuple : IIdBoxTuple
+{
+    private readonly TextBox _idBox;
+    private readonly TextBox _modelIdBox;
+    private readonly IEnumerable<TextBox> _idBoxes;
+
+    public IEnumerable<TextBox> IdBoxes => _idBoxes;
+
+    public TextBox IdBox => _idBox;
+    public TextBox ModelIdBox => _modelIdBox;
+
+    public IdBoxTuple()
+    {
+        _idBox = new();
+        _modelIdBox = new();
+        _idBoxes = [_idBox, _modelIdBox];
+    }
+}
