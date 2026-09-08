a8baeca9f4c9c6f07cd2b884997a017af8d186e9
lolchicer <lolchicer@live.ru>
2026-09-08 19:56:21 +0300

    тупанул и вместо конвертора сделал процедуры в гет-методе

--- src/View/Controls/CardIdBoxTuple.cs
+++ src/View/Controls/CardIdBoxTuple.cs
@@ -2,8 +2,11 @@
 
 namespace Lolchicer.Umlsql.View.Controls;
 
-public class CardIdBoxTuple : IIdBoxTuple<ViewModel.Documentational.Card>
+public class CardIdBoxTuple
+    : IIdBoxTuple<ViewModel.Documentational.Card>
 {
+    private CardGetter _getter;
+
     private readonly TextBox _idBox;
     private readonly TextBox _modelIdBox;
     private readonly IEnumerable<TextBox> _idBoxes;
@@ -11,12 +14,21 @@ public class CardIdBoxTuple : IIdBoxTuple<ViewModel.Documentational.Card>
     public IEnumerable<TextBox> IdBoxes => _idBoxes;
     
     public ViewModel.Documentational.Card Model
+    {
+        get
+        {
+            _getter.Id = Convert.ToInt32(_idBox.Text);
+            _getter.ModelId = Convert.ToInt32(_modelIdBox.Text);
+            return _getter.Model;
+        }
+    }
 
     public TextBox IdBox => _idBox;
     public TextBox ModelIdBox => _modelIdBox;
 
-    public CardIdBoxTuple()
+    public CardIdBoxTuple(CardGetter getter)
     {
+        _getter = getter;
         _idBox = new();
         _modelIdBox = new();
         _idBoxes = [_idBox, _modelIdBox];
--- src/View/Controls/ModelIdBoxTuple.cs
+++ src/View/Controls/ModelIdBoxTuple.cs
@@ -2,17 +2,30 @@
 
 namespace Lolchicer.Umlsql.View.Controls;
 
-public class ModelIdBoxTuple : IIdBoxTuple
+public class ModelIdBoxTuple
+    : IIdBoxTuple<ViewModel.Core.Model>
 {
+    private ModelGetter _getter;
+
     private readonly TextBox _idBox;
     private readonly IEnumerable<TextBox> _idBoxes;
 
     public IEnumerable<TextBox> IdBoxes => _idBoxes;
 
+    public ViewModel.Core.Model Model
+    {
+        get
+        {
+            _getter.Id = Convert.ToInt32(_idBox.Text);
+            return _getter.Model;
+        }
+    }
+
     public TextBox IdBox => _idBox;
 
-    public ModelIdBoxTuple()
+    public ModelIdBoxTuple(ModelGetter getter)
     {
+        _getter = getter;
         _idBox = new();
         _idBoxes = [_idBox];
     }
