d2cedf7c88a1253ce4c30df9c60fc3ff3cacad6e
lolchicer <lolchicer@live.ru>
2026-09-08 19:56:21 +0300

    тупанул и вместо конвертора сделал процедуры в гет-методе

--- src/View/Controls/ViewIdBoxTuple.cs
+++ src/View/Controls/ViewIdBoxTuple.cs
@@ -1,23 +1,36 @@
 ﻿using System.Windows.Controls;
 
-namespace Lolchicer.Umlsql.View.Controls
+namespace Lolchicer.Umlsql.View.Controls;
+
+public class ViewIdBoxTuple
+    : IIdBoxTuple<ViewModel.Core.View>
 {
-    public class ViewIdBoxTuple : IIdBoxTuple
+    private ViewGetter _getter;
+
+    private readonly TextBox _idBox;
+    private readonly TextBox _modelIdBox;
+    private readonly IEnumerable<TextBox> _idBoxes;
+
+    public IEnumerable<TextBox> IdBoxes => _idBoxes;
+
+    public ViewModel.Core.View Model
     {
-        private readonly TextBox _idBox;
-        private readonly TextBox _modelIdBox;
-        private readonly IEnumerable<TextBox> _idBoxes;
-
-        public IEnumerable<TextBox> IdBoxes => _idBoxes;
-
-        public TextBox IdBox => _idBox;
-        public TextBox ModelIdBox => _modelIdBox;
-
-        public ViewIdBoxTuple()
+        get
         {
-            _idBox = new();
-            _modelIdBox = new();
-            _idBoxes = [_idBox, _modelIdBox];
+            _getter.Id = Convert.ToInt32(_idBox.Text);
+            _getter.ModelId = Convert.ToInt32(_modelIdBox.Text);
+            return _getter.Model;
         }
+    }
+
+    public TextBox IdBox => _idBox;
+    public TextBox ModelIdBox => _modelIdBox;
+
+    public ViewIdBoxTuple(ViewGetter getter)
+    {
+        _getter = getter;
+        _idBox = new();
+        _modelIdBox = new();
+        _idBoxes = [_idBox, _modelIdBox];
     }
 }
