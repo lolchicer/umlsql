8ed42a79446f4a6b9f11723e23406fadd9263c15
lolchicer <lolchicer@live.ru>
2026-09-08 19:36:35 +0300

    "правильные" геттеры

---
+++ src/View/ControlCardModelGetter.cs
@@ -0,0 +1,16 @@
+﻿using Lolchicer.Umlsql.View.Controls;
+using Lolchicer.Umlsql.ViewModel;
+
+namespace Lolchicer.Umlsql.View;
+
+public class ControlCardModelGetter(
+    ApplicationContext context,
+    CardIdBoxTuple cardIdBoxTuple)
+    : IModelGetter<ViewModel.Documentational.Card>
+{
+    public ViewModel.Documentational.Card Model => context.Cards.Single(
+        card =>
+        card.Id == Convert.ToInt32(cardIdBoxTuple.IdBox.Text) &&
+        card.ModelId == Convert.ToInt32(cardIdBoxTuple.ModelIdBox.Text)
+    );
+}
---
+++ src/View/IModelGetter.cs
@@ -0,0 +1,6 @@
+﻿namespace Lolchicer.Umlsql.View;
+
+public interface IModelGetter<T> where T : ViewModel.Core.IModel
+{
+    public T Model { get; }
+}
