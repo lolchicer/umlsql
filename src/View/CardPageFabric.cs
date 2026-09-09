2198a7ec142b7db652af396d97e080c670a58802
lolchicer <lolchicer@live.ru>
2026-09-09 22:41:38 +0300

    проще так сделать

--- src/View/CardPageFabric.cs
+++ src/View/CardPageFabric.cs
@@ -2,7 +2,7 @@
 
 namespace Lolchicer.Umlsql.View;
 
-public class CardPageFabric(IModelGetter<ViewModel.Documentational.ICard> getter) : IModelPageFabric
+public class CardPageFabric(IModelGetter<ViewModel.Documentational.Card> getter) : IModelPageFabric
 {
     public ModelPage ModelPage => new(getter.Model);
 }
