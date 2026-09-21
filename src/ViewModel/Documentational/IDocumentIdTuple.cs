8f6505fe12db16c9b072ffda14b46bc094898535
lolchicer <lolchicer@live.ru>
2026-09-21 21:36:43 +0300

    idtuple

---
+++ src/ViewModel/Documentational/IDocumentIdTuple.cs
@@ -0,0 +1,30 @@
+﻿namespace Lolchicer.Umlsql.ViewModel.Documentational;
+
+public interface IDocumentIdTuple
+    : IIdTuple<Document>, Core.IViewIdTuple
+{
+    public new int Id { get; }
+    public new int ModelId { get; }
+
+    public new Document GetModel(
+        IEnumerable<Document> documents) =>
+        documents.First(
+            model =>
+            model.Id == Id &&
+            model.ModelId == ModelId);
+
+    Document IIdTuple<Document>.GetModel(IEnumerable<Document> documents) => GetModel(documents);
+
+    int Core.IViewIdTuple.Id => Id;
+    int Core.IViewIdTuple.ModelId => Id;
+}
+
+public readonly struct DocumentIdTuple
+    : IDocumentIdTuple
+{
+    public readonly int Id;
+    public readonly int ModelId;
+
+    int IDocumentIdTuple.Id => Id;
+    int IDocumentIdTuple.ModelId => ModelId;
+}
---
+++ src/ViewModel/Iterational/IRedactionIdTuple.cs
@@ -0,0 +1,30 @@
+﻿namespace Lolchicer.Umlsql.ViewModel.Iterational;
+
+public interface IRedactionIdTuple
+    : IIdTuple<Redaction>, Core.IViewIdTuple
+{
+    public new int Id { get; }
+    public new int ModelId { get; }
+
+    public new Redaction GetModel(
+        IEnumerable<Redaction> redactions) =>
+        redactions.First(
+            model =>
+            model.Id == Id &&
+            model.ModelId == ModelId);
+
+    Redaction IIdTuple<Redaction>.GetModel(IEnumerable<Redaction> redactions) => GetModel(redactions);
+
+    int Core.IViewIdTuple.Id => Id;
+    int Core.IViewIdTuple.ModelId => Id;
+}
+
+public readonly struct RedactionIdTuple
+    : IRedactionIdTuple
+{
+    public readonly int Id;
+    public readonly int ModelId;
+
+    int IRedactionIdTuple.Id => Id;
+    int IRedactionIdTuple.ModelId => ModelId;
+}
