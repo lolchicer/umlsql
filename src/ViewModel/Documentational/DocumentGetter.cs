55d7a01431c69e500575eefe35179b0310d40ddd
lolchicer <lolchicer@live.ru>
2026-09-21 21:14:42 +0300

    геттеры

---
+++ src/ViewModel/Documentational/DocumentGetter.cs
@@ -0,0 +1,7 @@
+﻿namespace Lolchicer.Umlsql.ViewModel.Documentational;
+
+public class DocumentGetter(ApplicationContext context, IIdTuple<Document> tuple)
+    : IModelGetter<Document>
+{
+    public Document Model => tuple.GetModel(context.Documents);
+}
---
+++ src/ViewModel/Iterational/RedactionGetter.cs
@@ -0,0 +1,7 @@
+﻿namespace Lolchicer.Umlsql.ViewModel.Iterational;
+
+public class RedactionGetter(ApplicationContext context, IIdTuple<Redaction> tuple)
+    : IModelGetter<Redaction>
+{
+    public Redaction Model => tuple.GetModel(context.Redactions);
+}
