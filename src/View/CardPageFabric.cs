ed43100e9114f974ff8dff8fb0aeb3b92596f14a
lolchicer <lolchicer@live.ru>
2026-09-21 22:46:15 +0300

    изменил pagefabric

--- src/View/CardPageFabric.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Lolchicer.Umlsql.View;
-
-public class CardPageFabric(ViewModel.IModelGetter<ViewModel.Documentational.Card> getter) : IModelPageFabric
-{
-    public ModelPage ModelPage => new(getter.Model);
-}
